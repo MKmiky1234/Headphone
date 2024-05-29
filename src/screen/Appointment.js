41591e4e29a92eb73e06d23bf15ecda8838521b1
MKmiky1234 <muskaan.s3356@gmail.com>
2024-05-29 03:13:55 -0700

    code

--- src/screen/Appointment.js
+++ src/screen/Appointment.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -7,14 +7,52 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Appointment = () => {
   return (
-    <View style={{backgroundColor:"#E6E9FF", height:"100%"}}>
-        <View style={{flexDirection: 'row', alignItems: 'center ' , marginTop:20 }}>
-      <Text  style={{fontSize:20, marginLeft:20 , flex:1, padding:20}}>Make an Appointment </Text>
+    <View style={{backgroundColor:"#E6E9FF",flex:1}}>
+        <View style={{ marginTop:20 ,padding:20, }}>
+            
+       <View style={{justifyContent:"space-between",flexDirection: 'row', alignItems: 'center ' ,}}>    
+      <Text style={{fontSize:20,color:"#07113D",fontFamily:"Poppins-Regular"}}>Make an Appointment </Text>
+      <View style={{backgroundColor:"white",flexDirection:'row',gap:10, }}>
+      <View>
       <AntDesign name="message1" color={"black"} size={25}  />
-      <Ionicons name="person-outline" color={"black"} size={25}  style={{backgroundColor:"white",}}/>
       </View>
+     <View>
+      <Ionicons name="person-outline" color={"black"} size={25}  />
+      </View>
+      </View>
+      
+      </View>
+      </View>
+      <View style={{height:1,width:"100%", backgroundColor:"#07113D"}}>
 
-
+      </View>
+      <View style={{ marginTop:20, padding:10}}>
+         <Text style={{fontSize:20, color:"#171E5B", fontFamily:"Poppins-Bold"}}>April 2020</Text>
+      </View>
+      <View style={{flexDirection: 'row',justifyContent:"space-between", padding:20,gap:10 }}>
+        <TouchableOpacity style={{ height:100, flexGrow:1, backgroundColor:"#FFFFFF",}}>
+       
+         <Text>MON</Text>
+        <Text>21</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ height:100, flexGrow:1, backgroundColor:"#FFFFFF", }}>
+        <Text>MON</Text>
+        <Text>21</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ height:100, flexGrow:1, backgroundColor:"#FFFFFF", }}>
+        <Text>MON</Text>
+        <Text>21</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ height:100, flexGrow:1, backgroundColor:"#FFFFFF", }}>
+        <Text>MON</Text>
+        <Text>21</Text>
+        </TouchableOpacity>  
+        <TouchableOpacity style={{ height:100, flexGrow:1, backgroundColor:"#FFFFFF", }}>
+        <Text>MON</Text>
+        <Text>21</Text>
+        </TouchableOpacity>  
+        </View>
+  
 
       </View>
   )
